3152b3bab4e90df4b7088178c19a608566cd24d2
isimbinadege <isimbi0123@gmail.com>
2025-09-28 22:54:39 +0300

    the start of js

---
+++ script.js
@@ -0,0 +1,9 @@
+let tasks = [
+    
+  { id: 1, name: "Finish assignment", dueDate: "2025-09-30", completed: false },
+  { id: 2, name: "Buy groceries", dueDate: "2025-10-01", completed: false },
+  { id: 3, name: "Call mom", dueDate: "2025-10-02", completed: true },
+  { id: 4, name: "Read a book", dueDate: "2025-10-03", completed: false },
+  { id: 5, name: "Workout", dueDate: "2025-10-04", completed: true }
+    
+];
